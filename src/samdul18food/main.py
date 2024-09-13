297f5a91b1f41d09de035a6cbdd22b99341395a9
Kimwonjoon <kimpass189@gmail.com>
2024-09-13 12:12:39 +0900

    cors

--- src/samdul18food/main.py
+++ src/samdul18food/main.py
@@ -1,10 +1,26 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 import time
 import pandas as pd
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
